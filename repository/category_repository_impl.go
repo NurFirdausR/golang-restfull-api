f6ab5cc6ae7b7a5f6ad7cdd1104dae39067b7961
NurFirdausR <nurfirdaus.5000@gmail.com>
2024-02-12 23:59:59 +0700

    commitment push

--- repository/category_repository_impl.go
+++ repository/category_repository_impl.go
@@ -1,4 +1,79 @@
 package repository
 
-// type CategoryRepositoryImpl struct {
-// }
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/NurFirdausR/golang-restfull-api/helper"
+	"github.com/NurFirdausR/golang-restfull-api/model/domain"
+)
+
+type CategoryRepositoryImpl struct {
+}
+
+func NewCategoryRepository() CategoryRepository {
+	return &CategoryRepositoryImpl{}
+}
+
+func (repository *CategoryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
+	SQL := "INSERT INTO category(name) value(?)"
+	result, err := tx.ExecContext(ctx, SQL, category.Name)
+	helper.PanicIfErr(err)
+	id, err := result.LastInsertId()
+	helper.PanicIfErr(err)
+
+	category.Id = int(id)
+	return category
+}
+
+func (repository *CategoryRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
+	SQL := "UPDATE category set name = ? where id = ?"
+	_, err := tx.ExecContext(ctx, SQL, category.Name, category.Id)
+	helper.PanicIfErr(err)
+	return category
+}
+func (repository *CategoryRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, category domain.Category) {
+	SQL := "DELETE  from category  where id = ?"
+	fmt.Println(category)
+	_, err := tx.ExecContext(ctx, SQL, category.Id)
+	helper.PanicIfErr(err)
+
+}
+
+func (repository *CategoryRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error) {
+	SQL := "SELECT * from category where id = ?"
+	rows, err := tx.QueryContext(ctx, SQL, categoryId)
+	helper.PanicIfErr(err)
+	defer rows.Close() // Close the rows when you're done with them
+
+	category := domain.Category{}
+	if rows.Next() {
+		err := rows.Scan(&category.Id, &category.Name)
+		helper.PanicIfErr(err)
+		return category, nil
+	} else {
+		return category, errors.New("category is not found")
+	}
+
+}
+
+func (repository *CategoryRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) []domain.Category {
+	SQL := "SELECT * from category"
+	rows, err := tx.QueryContext(ctx, SQL)
+	helper.PanicIfErr(err)
+	defer rows.Close() // Close the rows when you're done with them
+
+	var categories []domain.Category
+	for rows.Next() {
+		category := domain.Category{}
+		err := rows.Scan(&category.Id, &category.Name)
+		helper.PanicIfErr(err)
+		categories = append(categories, category)
+	}
+	err = rows.Err() // Check for any errors encountered during iteration
+	helper.PanicIfErr(err)
+
+	return categories
+}
